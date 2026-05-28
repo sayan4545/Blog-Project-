c0795133eca884923fc732776c940b926c0c9885
sayan4545 <sayanchatterjee3010@gmail.com>
2026-05-28 21:46:25 +0530

    Added Post entity

--- src/main/java/com/dev/sayan/learncode/blogsecurityproject/domain/entities/Post.java
+++ src/main/java/com/dev/sayan/learncode/blogsecurityproject/domain/entities/Post.java
@@ -1,4 +1,53 @@
 package com.dev.sayan.learncode.blogsecurityproject.domain.entities;
 
+import com.dev.sayan.learncode.blogsecurityproject.domain.enums.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String content;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus postStatus;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(content, post.content) && postStatus == post.postStatus && Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, postStatus, readingTime, createdAt, updatedAt);
+    }
 }
