f25ad0dc63b1a8593d0f47b2fc6aff88ebaaaef5
sayan4545 <sayanchatterjee3010@gmail.com>
2026-05-29 04:33:01 +0530

    Added ApiErrorResponse dto

--- src/main/java/com/dev/sayan/learncode/blogsecurityproject/domain/dtos/ApiErrorResponse.java
+++ src/main/java/com/dev/sayan/learncode/blogsecurityproject/domain/dtos/ApiErrorResponse.java
@@ -1,4 +1,29 @@
 package com.dev.sayan.learncode.blogsecurityproject.domain.dtos;
 
-public class ApiErrorResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiErrorResponse{
+    private int status;
+    private String message;
+    private List<FieldError> errors;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class FieldError{
+        private String field;
+        private String message;
+    }
+
 }
